f68603c82079586f000189d58caa6e708bec3ff9
Amit Kumar <47652949+amitkumarwastaken@users.noreply.github.com>
2019-02-26 07:37:13 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About(){
+	return(
+
+		<React.Fragment>
+			<h1>About</h1>
+			<p>This is a TodoList app built using React for UI(user interface) only version 1.0.0.
+			It is meant to learn, practice and get familiar with React js.</p>
+
+		</React.Fragment>
+
+
+
+		)
+}
+
+
+
+
+
+
+
+export default About;
---
+++ Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Header(){
+	return (
+
+		<header style={ headerStyle }>
+			<h1>TodoList</h1>
+			<Link to="/" style={{color: 'white'}}>Home</Link> | <Link to="/about" style={{color: 'white'}}>About</Link>
+		</header>
+
+		)
+
+
+
+}
+
+
+
+const headerStyle = {
+	background: '#333',
+	color: 'white',
+	textAlign: 'center',
+	padding: '10px'
+}
+
+
+export default Header;
